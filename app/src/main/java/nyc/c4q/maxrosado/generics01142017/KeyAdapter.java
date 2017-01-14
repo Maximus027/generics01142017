c199696ae35dce22f727a051032f7b9d7f4446a8
Max Rosado <max.a.rosado@gmail.com>
2017-01-14 17:54:40 -0500

    second commit

---
+++ app/src/main/java/nyc/c4q/maxrosado/generics01142017/KeyAdapter.java
@@ -0,0 +1,25 @@
+package nyc.c4q.maxrosado.generics01142017;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by maxrosado on 1/14/17.
+ */
+
+public class KeyAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/nyc/c4q/maxrosado/generics01142017/KeyViewHolder.java
@@ -0,0 +1,18 @@
+package nyc.c4q.maxrosado.generics01142017;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by maxrosado on 1/14/17.
+ */
+
+public class KeyViewHolder extends RecyclerView.ViewHolder {
+    public KeyViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    public void bind() {
+
+    }
+}
