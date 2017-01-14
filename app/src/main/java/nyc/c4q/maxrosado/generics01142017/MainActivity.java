e04d44fdd29899b123494fbb87f88e3b1630a162
Max Rosado <max.a.rosado@gmail.com>
2017-01-14 17:33:35 -0500

    First commit

---
+++ app/src/main/java/nyc/c4q/maxrosado/generics01142017/MainActivity.java
@@ -0,0 +1,13 @@
+package nyc.c4q.maxrosado.generics01142017;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
